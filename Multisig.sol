96ab2083403b9d570c4b5ac8e7fd36b771c9a67a
Derek Sorensen <derek-certora@Dereks-MacBook-Pro.local>
2024-10-29 11:54:59 +0200

    addValidator function implemented

--- Multisig.sol
+++ Multisig.sol
@@ -38,6 +38,23 @@ contract Multisig is State {
         uint256 newQuorum,
         uint256 _step
     ) public   {
+        // make sure validator list isn't maxed out
+        assert (validators.length < type(uint256).max ) ;
+
+        // check that sender is contract 
+        assert (msg.sender == address(this)) ; 
+
+        // append validator to validators list
+        validators.push(validator) ;
+        isValidator[validator] = true ;
+
+        // update quorum
+        quorum = newQuorum ;
+        
+        // update step
+        step = _step ;
+        
+        // TODO quorum has to be fibonacci of step (quorumIsValid)
     }
 
 
@@ -54,11 +71,21 @@ contract Multisig is State {
         address newValidator
     )
         public
-    {}
+    {
+        // replaceValidatorCaller
+        // * has to be reentrant 
+
+
+
+    }
 
     function changeQuorum(uint256 _quorum, uint256 _step)
         public
     {
+        // changeQuorumCaller
+        // * has to be a reentrant call 
+
+        // 
     }
 
     function transactionExists(bytes32 transactionId)
@@ -75,16 +102,32 @@ contract Multisig is State {
         bytes calldata data,
         bool hasReward
     ) public payable {
+        // lengthChangeOnlyInAddValidatorActivation
+        // one of three methods that can increase length of execute transaction
+
+        // lengthChangeWorks2
+        // has to have at least one example 
+
+        // voteForTransactionCaller
+        // * can only be done by a validator 
+
+        // 
     }
 
     function executeTransaction(bytes32 transactionId) public
     {
+        // lengthChangeOnlyInAddValidatorActivation
+        // one of three methods that can increase length of execute transaction
+
+        // lengthChangeWorks3
+        // has to have at least one example 
     }
 
     function removeTransaction(bytes32 transactionId) public {
     }
 
     function isConfirmed(bytes32 transactionId) public view returns (bool) {
+        // has enough votes for the quorum?
     }
 
     function getDataOfTransaction(bytes32 id) external view returns (bytes memory data){
@@ -102,6 +145,8 @@ contract Multisig is State {
         returns (uint256 count)
     {
 
+        // addValidatorFunctinality4
+        // * count cannot change when adding a validaot
     }
 
     function distributeRewards() public reentracy
